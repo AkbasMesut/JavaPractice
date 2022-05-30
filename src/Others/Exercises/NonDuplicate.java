95e492107403cfefcfa4937c1e2fc8bd9d887aac
makbas06 <mutluates135@gmail.com>
2022-05-30 15:58:40 +0300

    update

---
+++ src/Others/Exercises/NonDuplicate.java
@@ -0,0 +1,29 @@
+package Others.Exercises;
+
+public class NonDuplicate {
+
+    public static void main(String[] args) {
+       // Scanner scan = new Scanner(System.in);
+        int[] score = {1, 1, 2, 1, 3, 3, 4, 2};
+
+
+        for (int i = 0; i < score.length; i++) {
+
+            int unique = 0;
+            for (int j = 0; j < score.length; j++) {
+
+                if (i != j && score[i] == score[j]) {
+                    unique++; }
+
+            }
+
+            if (unique == 0) {
+                System.out.println(score[i]);
+                break;
+            }
+
+        }
+
+    }
+
+}
---
+++ src/Others/Exercises/maxLenghtString.java
@@ -0,0 +1,23 @@
+package Others.Exercises;
+
+public class maxLenghtString {
+
+    public static void main(String[] args) {
+
+        String[] words = {"aaadfsdfa","bbbbbb","whatsuppppp","jjaaaavvvaaaaaa"};
+
+        int maxIndex = 0;
+        for (int i = 0; i < words.length; i++) {
+
+            if (words[i].length() > words[maxIndex].length()){
+
+                maxIndex =i;
+            }
+        }
+
+        System.out.println(words[maxIndex]);
+
+
+    }
+
+}
